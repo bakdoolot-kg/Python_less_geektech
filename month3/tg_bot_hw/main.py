1707ecb4f6ee0be6ee4ce6f0cd227c85ced3b21d
bakdoolot <altybaevbaku15@gmail.com>
2022-06-23 16:23:48 +0600

    Done HW 5

--- month3/tg_bot_hw/main.py
+++ month3/tg_bot_hw/main.py
@@ -1,18 +1,22 @@
+import asyncio
+
 from aiogram.utils import executor
 
 from config import dp
-from handlers import extra, client, callback, admin, fsmAdminMenu
+from handlers import extra, client, callback, admin, fsmAdminMenu, notification
 import logging
 from database.bot_db import sql_create
 
 
 async def on_startup(_):
+    asyncio.create_task(notification.scheduler())
     sql_create()
 
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
 fsmAdminMenu.register_handlers_fsm(dp)
+notification.register_handler_notification(dp)
 extra.register_handlers_extra(dp)
 
 
