4a4e798ba70f50048520045dd76aa5d51a03cf41
bakdoolot <altybaevbaku15@gmail.com>
2022-07-04 16:21:16 +0600

    add inline search wiki

--- handlers/inline.py
+++ handlers/inline.py
@@ -22,9 +22,24 @@ async def inline_youtube_handler(query: types.InlineQuery):
             )
         ) for link in links
     ]
+    await query.answer(articles, cache_time=60, is_personal=True)
 
+
+async def inline_wiki_handler(query: types.InlineQuery):
+    text = query.query or 'echo'
+    link = f"https://ru.wikipedia.org/wiki/{text}"
+    result_id: str = hashlib.md5(text.encode()).hexdigest(),
+    articles = [types.InlineQueryResultArticle(
+        id=result_id,
+        title="WIKI: ",
+        url=link,
+        input_message_content=types.InputMessageContent(
+            message_text=link
+        )
+    )]
     await query.answer(articles, cache_time=60, is_personal=True)
 
 
 def register_inline_handler(dp: Dispatcher):
     dp.register_inline_handler(inline_youtube_handler)
+    # dp.register_inline_handler(inline_wiki_handler)
