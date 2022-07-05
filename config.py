aa1904a834c26fc634abac63a653980e4b080d9c
bakdoolot <altybaevbaku15@gmail.com>
2022-07-05 12:25:34 +0600

    added postgres

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
 ADMIN = [608718247]
 URL = "https://baku18-1.herokuapp.com/"
+URI = "postgres://ekmzrrbkxdxyum:0d73c0f56e6f6cb811dcc29a27b2d82fccb1932c06559904848732f9cbcc4ae3@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/dej61pdm5tbe41"
--- handlers/client.py
+++ handlers/client.py
@@ -5,6 +5,7 @@ from config import bot
 from keyboards.client_kb import start_markup
 from database.bot_db import sql_command_random
 from parser import movies
+from database import psql_db
 
 
 # @dp.message_handler(commands=['start'])
@@ -58,8 +59,29 @@ async def parser_movies(message: types.Message):
         )
 
 
+async def hi(message: types.Message):
+    user_id = message.from_user.id
+    username = f"@{message.from_user.username}"
+    fullname = message.from_user.full_name
+    try:
+        psql_db.cursor.execute(
+            "INSERT INTO users (id, username, fullname) VALUES (%s, %s, %s)",
+            (user_id, username, fullname)
+        )
+        psql_db.cursor.commit()
+
+    except:
+        psql_db.cursor.execute("rollback")
+        psql_db.cursor.execute(
+            "INSERT INTO users (id, username, fullname) VALUES (%s, %s, %s)",
+            (user_id, username, fullname)
+        )
+        psql_db.cursor.commit()
+
+
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(command_start, commands=['start'])
     dp.register_message_handler(quiz_1, commands=['quiz'])
     dp.register_message_handler(show_random_user, commands=['random'])
     dp.register_message_handler(parser_movies, commands=['film'])
+    dp.register_message_handler(hi, commands=['hi'])
