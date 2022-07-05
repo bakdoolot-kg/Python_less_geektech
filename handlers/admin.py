2c052e3834b1341336c1217f51ab87875b8fedd7
bakdoolot <altybaevbaku15@gmail.com>
2022-07-05 12:39:41 +0600

    added users command

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,6 +1,7 @@
 from aiogram import types, Dispatcher
 from config import bot, ADMIN
 from database.bot_db import sql_commands_get_all_id
+from database import psql_db
 
 
 async def ban(message: types.Message):
@@ -27,6 +28,20 @@ async def mailing(message: types.Message):
         await message.answer("Ты не мой БОСС!!!")
 
 
+async def get_all_users(message: types.Message):
+    if message.from_user.id in ADMIN:
+        all_users = psql_db.cursor.execute("SELECT * FROM users")
+        result = psql_db.cursor.fetchall()
+        answer = ""
+        for row in result:
+            answer += f"ID {row[0]}\nUsername: {row[1]}\nFullname: {row[2]}\n\n"
+        answer += f"{len(result)} users"
+        await message.answer(answer)
+    else:
+        await message.answer("Ты не мой БОСС!!!")
+
+
 def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(ban, commands=["ban"], commands_prefix="!/")
     dp.register_message_handler(mailing, commands=["R"])
+    dp.register_message_handler(get_all_users, commands=["users"])
